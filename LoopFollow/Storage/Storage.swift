01900a6fb762c8c9f51c761ba3be2c09ad117bad
codebymini <daniel@codebymini.se>
2025-12-04 20:25:51 +0100

    Add bolus calculator

--- LoopFollow/Storage/Storage.swift
+++ LoopFollow/Storage/Storage.swift
@@ -178,6 +178,9 @@ class Storage {
     var bolusIncrement = SecureStorageValue<HKQuantity>(key: "bolusIncrement", defaultValue: HKQuantity(unit: .internationalUnit(), doubleValue: 0.05))
     var bolusIncrementDetected = StorageValue<Bool>(key: "bolusIncrementDetected", defaultValue: false)
 
+    // Recent carb entry for bolus calculator
+    var recentCarbEntry = StorageValue<RecentCarbEntry?>(key: "recentCarbEntry", defaultValue: nil as RecentCarbEntry?)
+
     static let shared = Storage()
     private init() {}
 }
